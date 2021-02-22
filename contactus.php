487042a3291f1527bd3d5bf54e37aa62a76d10a8
rbaia28 <baia5230@gmail.com>
2021-02-22 15:46:21 -0500

    added email function to connect with us. Included link for guidance and nots

--- contactus.php
+++ contactus.php
@@ -2,12 +2,19 @@
 <html>
 <head>
     <link href="style.css" type="text/css" rel="stylesheet">
+    <title>Connect with Us!</title>
 </head>
 <body>
-    <?php
-    echo "my first PHP script!";
-    ?>
-
+    <main>
+        <h1> Connect with Us! </h1></br>
+        <p>Send E-mail</p>
+        <form class = "contact-form" action= "contactus.php" method="post">
+            <input type="text" name="name" placeholder="Full Name">
+            <input type="text" name="mail" placeholder="Your Email">
+            <input type="text" name="subject" placeholder="Subject">
+            <textarea name= "message" placeholder="Message"></textarea>
+            <button type="submit" name="submit">Send Mail</button>
+        </form>
 
 </body>
 </html>
