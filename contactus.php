1173aee0be601881b7743a22dfd27647da641f67
rbaia28 <baia5230@gmail.com>
2021-03-01 20:43:28 -0500

    updated php form to stay on page and not delete content if incorrect. will not stay if refresh though. combinded php pages to one and delete extra

--- contactus.php
+++ contactus.php
@@ -1,11 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Connect with Us!</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/style.css" rel="stylesheet">
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
@@ -18,7 +24,6 @@
                     <a class="nav-link" href="ourstory.html">Our Story</a>
                 </li>
 
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="contactus.php">Connect with Us!</a>
                 </li>
@@ -42,17 +47,60 @@
             </div>
         </div>
     </nav>
-    <main>
+    <div id="form" name="form">
+        <?php
+        $fullname = '';
+        $email = '';
+        $subject = '';
+        $message = '';
+        $mailFrom = '';
+
+    if (isset($_Post['submit-btn'])) {
+
+         if (!empty(trim($_POST['name']))) {
+            $fullname = trim($_POST['name']);
+        } else {
+            echo "<p> Name cannot be blank</p>";
+        }
+
+        if (!empty(trim($_POST['subject']))) {
+            $subject = trim($_POST['subject']);
+        } else {
+            echo "<p>Subject cannot be blank</p>";
+        }
+
+        if (!empty(trim($_POST['message']))) {
+            $message = trim($_POST['message']);
+        } else {
+            echo "<p>Message cannot be blank</p>";
+        }
+
+        if (isset($_POST['submit'])) {
+            $fullname = $_POST['name'];
+            $subject = $_POST['subject'];
+            $mailFrom = $_POST['mail'];
+            $message = $_POST['message'];
+            $mailTo = "gaganchhabra24@yahoo.com";
+            $headers = "From: " . $mailFrom;
+            $txt = "You have recieved an email from " . $fullname . ".\n\n" . $message;
+            mail($mailTo, $subject, $txt, $headers);
+            header("Location: contactus.php?mailsend");
+        }
+    }
+        ?>
+
         <h1> Connect with Us! </h1></br>
-        <p>Send E-mail</p>
+        <p>Send E-mail</p><br/>
         <form class="contact-form" action="contactus.php" method="post">
-            <input type="text" name="name" placeholder="Full Name">
-            <input type="email" name="mail" placeholder="Your Email">
-            <input type="text" name="subject" placeholder="Subject">
-            <textarea name="message" placeholder="Message"></textarea>
-            <button type="submit" name="submit">Send Mail</button>
+            <input type="text" name="full name" <?= ($fullname ? 'value="' . $fullname . '"' :  ''); ?> placeholder="Full Name"required><br/>
+            <input type="email" name="mail" <?= ($email ? 'value="' . $email . '"' : ''); ?> placeholder="Your Email" required><br/>
+            <input type="text" names="subject" <?=($subject ? 'value="' . $subject . '"' : ''); ?> placeholder="Subject" required></br/>
+            <textarea name="message" placeholder="Tell us about your experience! Better yet..dont." required><?= ($message ? $message : ''); ?></textarea><br/>
+            <input type="submit" name="submit-btn"></br>
         </form>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<--add checkbox for subscribe for coups
 </body>
 
 </html>
